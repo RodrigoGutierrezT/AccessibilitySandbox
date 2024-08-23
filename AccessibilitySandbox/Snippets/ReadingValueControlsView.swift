11399b3c26d6ce2c0703f638e925871a5452e888
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-08-22 20:40:14 -0400

    feat: Add ReadingValueControlsView

---
+++ AccessibilitySandbox/Snippets/ReadingValueControlsView.swift
@@ -0,0 +1,43 @@
+//
+//  ReadingValueControlsView.swift
+//  AccessibilitySandbox
+//
+//  Created by Rodrigo on 22-08-24.
+//
+
+import SwiftUI
+
+struct ReadingValueControlsView: View {
+    @State private var value = 10
+    
+    var body: some View {
+        VStack {
+            Text("Value \(value)")
+            
+            Button("Increment") {
+                value += 1
+            }
+            
+            Button("Decrement") {
+                value -= 1
+            }
+        }
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled")
+            }
+        }
+    }
+}
+
+#Preview {
+    ReadingValueControlsView()
+}
